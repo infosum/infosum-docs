ad4d3a7ba55bd6f5cf8395ef411827312cd37c24
Al Stockdill-Mander <allan.stockdill-mander@infosum.com>
2023-12-13 11:38:34 +0000

    center api reference

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -32,7 +32,7 @@ const FeatureList: FeatureItem[] = [
 
 function Feature({title, Svg, description}: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx('col')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
